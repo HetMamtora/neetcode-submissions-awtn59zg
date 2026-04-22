60244d47c05db4488109f87dbb3078d3219a6521
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-04-22 20:49:07 +0530

    Add: palindrome-linked-list - submission-5

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-5.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    //helper
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //reverse from helper
+        ListNode rev = reverse(slow.next);
+        slow.next = null;
+
+        while(rev != null)
+        if(rev.val != head.val){
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return true;
+}
