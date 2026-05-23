4cb4a0238677f726f05f53031eda72a3a4bdaeb7
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-05-23 18:33:54 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.java
@@ -0,0 +1,58 @@
+class Solution {
+    public void solve(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+
+        //Top Row
+        for(int i = 0; i < col; i++){
+            if(board[0][i] == 'O'){
+                dfs(board, 0, i, row, col);
+            }
+        }
+        //Bottom Row
+        for(int i = 0; i < col; i++){
+            if(board[row - 1][i] == 'O'){
+                dfs(board, row - 1, i, row, col);
+            }
+        }
+
+        //Left Column
+        for(int i = 0; i < row; i++){
+            if(board[i][0] == 'O'){
+                dfs(board, i, 0, row, col);
+            }
+        }
+        //Right Column
+        for(int i = 0; i < row; i++){
+            if(board[i][col - 1] == 'O'){
+                dfs(board, i, col - 1, row, col);
+            }
+        }
+        for(int r = 0; r < row; r++){
+            for(int c = 0; c < col; c++){
+                if(board[r][c] == 'O'){
+                    board[r][c] = 'X';
+                }
+                if(board[r][c] == 'T'){
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int r, int c, int row, int col){
+        board[r][c] = 'T';
+
+        int[] dRow = {-1, 1, 0, 0};
+        int[] dCol = {0, 0, -1, 1};
+
+        for(int d = 0; d < 4; d++){
+            int newRow = r + dRow[d];
+            int newCol = c + dCol[d];
+
+            if(newRow >= 0 && newRow < row && newCol >= 0 && newCol < col && board[newRow][newCol] == 'O'){
+                dfs(board, newRow, newCol, row, col);
+            }
+        }
+    }
+}
