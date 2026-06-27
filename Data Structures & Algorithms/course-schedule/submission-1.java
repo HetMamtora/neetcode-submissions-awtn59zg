9bdfdf91de71e2d63c86801ec31f5314b2a27c57
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-06-27 19:48:33 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            adjacency.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int[] pre : prerequisites){
+            adjacency.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        int count = 0;
+        while(!queue.isEmpty()){
+            int result = queue.poll();
+            count++;
+
+            for(int neighbors : adjacency.get(result)){
+                indegree[neighbor]--;
+
+                if(indegree[neighbor] == 0){
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        if(count == numCourses){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
