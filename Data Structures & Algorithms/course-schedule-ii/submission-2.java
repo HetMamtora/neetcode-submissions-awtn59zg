e007ed5634928759e4312ff7fe4f2a99e54eefb4
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-06-27 20:12:49 +0530

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            adjacency.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int[] pre : prerequisites){
+            adjacency.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        int count = 0;
+        int[] result - new int[numCourses];
+        int index = 0;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            result[index++] = node;
+            count++;
+
+            for(int neigbor : adjacency.get(node)){
+                indegree[neighbor]--;
+
+                if(indegree[neighbor] == 0){
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        if(count == numCourses){
+            return result;
+        } else {
+            return new int[]{};
+        }
+    }
+}
