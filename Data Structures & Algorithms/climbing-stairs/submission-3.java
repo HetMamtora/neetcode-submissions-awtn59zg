a904730fd71bf36460188440a91a2b1b2e6601d6
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-08-17 20:03:06 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        //Memoization
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return helper(n, memo); 
+    }
+
+    public int helper(int n, int[] memo){
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+
+        int result = helper(n - 1, memo) + helper(n - 2, memo);
+        memo[n] = result;
+
+        return memo[n];
+    }
+}
