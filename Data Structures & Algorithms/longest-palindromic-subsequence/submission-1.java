f635c527bddfff1ad579c52c1df28be69ee8c4fe
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2026-09-24 23:24:00 +0530

    Add: longest-palindromic-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int m = s.length();
+        String rev = new StringBuilder(s).reverse().toString();
+        int n = rev.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == rev.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
